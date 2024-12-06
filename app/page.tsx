3c7b03476a5a34b19daa9771c6656f3a255be321
Nisaru Abak <nizar.abak@live.nl>
2024-12-06 18:01:50 +0100

    add grid-area's instead of grid-column in items

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import './style.css';
 import Image from 'next/image';
 import KarmaIcon from '../public/images/icon-karma.svg';
+import Super from '../public/images/icon-supervisor.svg';
+import Calc from '../public/images/icon-calculator.svg';
+import Team from '../public/images/icon-team-builder.svg';
 export default function Home() {
   return (
  <body>
@@ -16,22 +19,22 @@ export default function Home() {
      <div className="items super"><h2>Super visor</h2>
          <p>Monitors activity to identify project roadblocks|</p>
          <div className="img-container">
-             <Image src={KarmaIcon} alt="Karma Icon"  className="icn"/>
+             <Image src={Super} alt="Karma Icon"  className="icn"/>
          </div>
      </div>
-     <div className="items team">Team Build
+     <div className="items team">   <h2>Team Build</h2>
          <p>Monitors activity to identify project roadblocks|</p>
          <div className="img-container">
-             <Image src={KarmaIcon} alt="Karma Icon"  className="icn"/>
+             <Image src={Team} alt="Karma Icon"  className="icn"/>
          </div>
      </div>
-     <div className="items calc">Calculator
+     <div className="items calc"><h2>Calculator</h2>
          <p>Monitors activity to identify project roadblocks|</p>
          <div className="img-container">
-             <Image src={KarmaIcon} alt="Karma Icon"  className="icn"/>
+             <Image src={Calc} alt="Karma Icon"  className="icn"/>
          </div>
      </div>
-     <div className="items karma">Karma
+     <div className="items karma"><h2>Karma</h2>
          <p>Monitors activity to identify project roadblocks</p>
          <div className="img-container">
              <Image src={KarmaIcon} alt="Karma Icon"  className="icn"/>
